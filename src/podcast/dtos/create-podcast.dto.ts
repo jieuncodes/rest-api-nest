f6aae94bb0cde9aa3c6940e70d63432e7a3be23a
youredith <110220308+jieuncodes@users.noreply.github.com>
2023-11-09 00:57:55 +0900

    Setup validation pipe

--- src/podcast/dtos/create-podcast.dto.ts
+++ src/podcast/dtos/create-podcast.dto.ts
@@ -1,10 +1,13 @@
 import { ArgsType, Field } from '@nestjs/graphql';
+import { IsString } from 'class-validator';
 
 @ArgsType()
 export class CreatePodcastDto {
   @Field((type) => String)
+  @IsString()
   title: string;
 
+  @Field((type) => String)
   @Field((type) => String)
   category: string;
 }
